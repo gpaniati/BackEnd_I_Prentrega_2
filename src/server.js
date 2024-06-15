427abc43658f1715a6e01e28d8d68adcdc60c130
gpaniati <gpaniati@gmail.com>
2024-06-15 08:47:37 -0300

    Estructura basica websocket

--- src/server.js
+++ src/server.js
@@ -1,10 +1,66 @@
 import express from "express";
+import paths from "./utils/paths.js";
+import handlebars from "./config/handlebars.config.js";
+import serverSocket from "./config/socket.config.js";
+import homeRouter from "./routes/home.router.js";
+/*
 import cartsRouter from "./routes/carts.router.js"
 import productsRouter from "./routes/products.router.js"
-
+*/
 const server = express();
 const PORT = 8080;
 const HOST = "localhost";
+
+// Definición de enrutadores
+server.use("/chat", homeRouter);
+
+// Configuración del motor de plantillas
+handlebars.config(server);
+
+// Declaración de ruta estática: http://localhost:8080/api/public
+server.use("/api/public", express.static(paths.public));
+
+// Control de rutas inexistentes
+server.use("*", (req, res) => {
+   res.status(404).send("<h1>Error 404</h1><h3>La URL indicada no existe en este servidor</h3>");
+});
+
+// Control de errores internos
+server.use((error, req, res) => {
+   console.log("Error:", error.message);
+   res.status(500).send("<h1>Error 500</h1><h3>Se ha generado un error en el servidor</h3>");
+});
+
+// Método oyente de solicitudes
+const serverHTTP = server.listen(PORT, () => {
+   console.log(`Ejecutándose en http://${HOST}:${PORT}`);
+});
+
+// Configuración del servidor de websocket
+serverSocket.config(serverHTTP);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+
+import cartsRouter from "./routes/carts.router.js"
+import productsRouter from "./routes/products.router.js"
+
+
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
