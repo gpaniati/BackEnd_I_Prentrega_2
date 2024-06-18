aad96e2b152b2c308d3f8d5134a409d738470c85
gpaniati <gpaniati@gmail.com>
2024-06-18 00:34:58 -0300

    crearProducto v1.2

--- src/public/js/realTimeProducts.js
+++ src/public/js/realTimeProducts.js
@@ -29,53 +29,94 @@ const renderizarProductos = (productos) => {
     });
  }
 
-//Elimina un producto de la base de productos.
+//Elimina un producto de la base de productos (ONCLICK del boton eliminar de cada producto).
 const eliminarProducto = (id) => {
     socket.emit("eliminar-producto", id);
 }
 
+//Crea un nuevo producto en base a los datos ingredados en el formulario.
 const crearProducto = () => {
     const entradasFormulario = document.getElementById('form-crearProducto');
-    for (let i = 0; i < entradasFormulario.length; i++) {
-        if (entradasFormulario[i].type !== 'button'){
-            console.log(entradasFormulario[i].value);
+    let i = 0;
+    let faltanDatosEntrada = false;
+
+    //Valida que se informen todos los datos de entrada.
+    while ((i < entradasFormulario.length)&&(!faltanDatosEntrada)) {
+        if (entradasFormulario[i].type !== "button"){
+            if(entradasFormulario[i].value.trim() == ""){
+                Swal.fire({
+                    icon: "warning",
+                    title: "Datos de entrada incompletos",
+                    html: `Falta carga el campo <b>${entradasFormulario[i].id}</b>!`
+                });
+                faltanDatosEntrada = true;
+            }
         }
+        i++;
+    }
+    if (!faltanDatosEntrada) {
+        //Defino nuevo producto.
+        const nuevoProducto = {
+        title: (document.getElementById("titulo")).value,
+        description: (document.getElementById("descripcion")).value,
+        code: (document.getElementById("codigo")).value,
+        price: (document.getElementById("precio")).value,
+        status: true,
+        stock: (document.getElementById("stock")).value,
+        category: (document.getElementById("categoria")).value,
+        thumbnails: "https://i.ibb.co/BCP4y5H/modofit-producto-sin-imagen.png"
+        };
+
+        //Limpiar formulario.
+        for (i = 0; i < entradasFormulario.length; i++){
+            if (entradasFormulario[i].type == "text"){
+                entradasFormulario[i].value = "";
+            }
+        }
+
+        socket.emit("crear-producto", nuevoProducto);
     }
 };
 
 //Notificación de producto eliminado a todos los clientes conectados al servidor.
-//Envio de render a todos los clientes conectados.
 socket.on("producto-eliminado-resto", (id) => {
     Swal.fire({
-        toast: true,
         position: "top-end",
-        timer: 2000,
-        timeProgressBar: true,
+        icon: "warning",
         title: `Han eliminado el producto ${id} de la base`,
-        icon: "success",
+        showConfirmButton: false,
+        timer: 1500
     });
 });
 
-//Notificación de producto eliminado al cliente q lo eliminó.
-socket.on("producto-eliminado-autor", (id) => {
+//Notificación de producto eliminado al cliente que lo eliminó.
+socket.on("producto-eliminado-autor", () => {
     Swal.fire({
-        toast: true,
         position: "top-end",
-        timer: 2000,
-        timeProgressBar: true,
-        title: `Han eliminado el producto ${id} de la base`,
         icon: "success",
+        title: "Producto eliminado correctamente",
+        showConfirmButton: false,
+        timer: 1500
     });
 });
 
+//Notificación de producto credo al cliente que lo creó.
+socket.on("producto-creado-autor", () => {
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Producto creado correctamente",
+        showConfirmButton: false,
+        timer: 1500
+    });
+});
 
-//Renderiza los productos de la base cuando se conecta un cliente.
+//Renderiza los productos de la base cuando se conecta un cliente nuevo.
 socket.on("cliente-conectado", (productos) => {
     renderizarProductos(productos);
 });
 
-//Renderiza los productos de la base frente a la accion de algun cliente sobre la misma (Eliminar producto/ Añadir Producto).
+//Renderiza los productos de la base frente a la accion de algun cliente sobre la misma (Eliminar producto/ Crear Producto).
 socket.on("renderizar-base", (productos) => {
-    console.log(productos);
     renderizarProductos(productos);
 });
